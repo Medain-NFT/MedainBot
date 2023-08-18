7d358e9ea313bf6d0465e0ec254c6606b0cd0dfe
Nivin Vysakh <nivinsvysakh2020@outlook.com>
2023-08-18 21:58:59 +0530

    More Functions
    Added A New Option called ` Collab` and New Embed and New comand `!medainstatus`

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 import os
 import dotenv
 
-from discord.ui import Select , View
-from discord.ext import commands
+from discord.ui import Select , View 
+from discord.ext import commands 
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,24 +14,41 @@ prefix = "!"
 bot = commands.Bot(command_prefix=prefix,intents=intents)
 
 
+
 # Bot Triggers
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="MedainNFT!"), status=discord.Status.do_not_disturb)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Medain Multiverse"), status=discord.Status.idle)
     print("Bot Is Online")
+
+
+
+
+
+embed=discord.Embed(title="Help Wizard ", url="https://github.com/Medain-NFT", description="Hello, I am Medain Assist and i am Super Glad to help you . Choose a Option from this Embed Message and i will provide you the Help ", color=0xf67104)
+embed.set_author(name="Medain Network")
+embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1101145432941404166/1139544491716526253/dcku_004.png")
+embed.add_field(name="Help ", value="Help in any Other Stuffs", inline=True)
+embed.add_field(name="Promotions", value="Are you looking to promote your product or service, regardless of its size? Look no further! We have you covered. Our advertising plans are designed to meet your needs, starting from just a day of promotion. We offer three comprehensive plans to help you reach your target audience effectively.", inline=True)
+embed.add_field(name="ChangeLogs", value="A Clear Change Log list of Medain Assitent Bot and Medain Guard Bot ", inline=True)
+embed.add_field(name="Medain NFT", value="Explore our Medain NFT by purchasing our NFT Collection in Open Sea ", inline=True)
+embed.add_field(name="Medain Status ", value="Explore our Status Page to Ensure that our Bot is Working Properly and Smoothly To use just type `!medainstatus`", inline=True)
+embed.add_field(name="Collab", value="You can Collab the Bot by adding more functions and New Stuffs for the bot .", inline=True)
+embed.set_footer(text="This Embed is Prepared and hooked into the bot in 11/8/2023")
 
 
 class Select(discord.ui.Select):
     def __init__(self):
         options=[
-            discord.SelectOption(label="Tickets",emoji="<:points:1135400687749050439>",description="Need Help in Tickets." ),
-            discord.SelectOption(label="Help ",emoji="<:points:1135400687749050439>",description="Help in Payments or Medain Guild."),
-            discord.SelectOption(label="Promotions",emoji="<:points:1135400687749050439>",description="Know About Promotions."),
-            discord.SelectOption(label="ChangeLogs",emoji="<:points:1135400687749050439>",description="Know About Bot Changelogs"),
-            discord.SelectOption(label="Medain NFT",emoji="<:points:1135400687749050439>",description="See Our Medain NFT !")
+            discord.SelectOption(label="Tickets",emoji="<:Ticket:1139953355070898296>",description="Need Help in Tickets." ),
+            discord.SelectOption(label="Help ",emoji="<:help:1139953548063412284>",description="Help in Payments or Medain Guild."),
+            discord.SelectOption(label="Promotions",emoji="<:Promotions:1139954508575801425>",description="Know About Promotions."),
+            discord.SelectOption(label="ChangeLogs",emoji="<:logs:1140679922067701810>",description="Know About Bot Changelogs"),
+            discord.SelectOption(label="Medain NFT",emoji="<:cripto:1140680107762139188>",description="See Our Medain NFT !"),
+            discord.SelectOption(label="Collab",emoji="<:collaborazioni:1142128813690589264>",description="You add more Functions to the Bot By Collab in Github.")
             ]
-        super().__init__(placeholder="Select a Option",max_values=1,min_values=1,options=options)
+        super().__init__(placeholder="Select a Help Service",max_values=1,min_values=1,options=options)
     async def callback(self, interaction: discord.Interaction):
         if self.values[0] == "Tickets":
             await interaction.response.send_message(content=" <#1130396102772916297> Read our Guildes. If There is a Issue contact <@&1126118005760327684>",ephemeral=True)
@@ -43,16 +60,27 @@ class Select(discord.ui.Select):
             await interaction.response.send_message(" See the Change Logs : https://github.com/Medain-NFT/MedainBot/releases\n Updated and Maintained by : <@957196694393614367>.")
         elif self.values[0] == "Medain NFT":
             await interaction.response.send_message("Medain NFT Collection \n Link : https://opensea.io/collection/medain")
+        elif self.values[0] == "Collab":
+            await interaction.response.send_message("Hello There User Seems that You wanted to collab me . Your Collab Makes this bot More Good and Safe Bot For the Community . \n **How to Contribute**\n- You can Contribute the Bot By forking the Repo \n - Make Changes to File and Add some new Functions \n - Send a Pull Request in Gihub \n  **Note** \n - Please Note all the Changes in the Pull Requset Message So that Devs Can Understand the Changes Properly . ")
 class SelectView(discord.ui.View):
     def __init__(self, *, timeout = 180):
         super().__init__(timeout=timeout)
         self.add_item(Select())
 
 
-
 @bot.command()
 async def medain(ctx):
+    
     view = Select()
-    await ctx.send('Hello User How can i help your Day.\nChoose a Option from the DropDown Menu 🥰', view=SelectView())
+    await ctx.send(embed=embed,view=SelectView())
+
+
+
+
+@bot.command()
+async def medainstatus(ctx):
+    emoji = bot.get_emoji(1139954508575801425)
+    await ctx.send(f'To See our Latest Status just Visit our Status Page {emoji} \n https://medainbot.statuspage.io/')
+
 
 bot.run(os.getenv('tock'))
